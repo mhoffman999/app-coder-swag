592efb9eea878b0686334713558f7a2f077ec565
Michael Hoffman <mhoffman999@gmail.com>
2018-12-23 14:45:38 -0500

    Began creation of main controller and added customer tableview cell file.

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Michael Hoffman on 12/23/18.
+//  Copyright © 2018 Here We Go. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
